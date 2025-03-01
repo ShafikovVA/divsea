ea007d5203502a53e205537155570675cb527603
Vladislav <vshafikov@list.ru>
2025-03-01 15:57:35 +0500

    feat: Explore Marketplace block

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/blocks/Home/Hero/Hero";
 import WeeklyTop from "@/components/blocks/Home/WeeklyTop/WeeklyTop";
 import TopCollection from "@/components/blocks/Home/TopCollection/TopCollection";
+import ExploreMarketplace from "@/components/blocks/Home/ExploreMarketplace/ExploreMarketplace";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Hero />
       <WeeklyTop />
       <TopCollection />
-
+      <ExploreMarketplace />
     </>
   );
 }
